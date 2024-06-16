8015fb1be7d616a1ad55e37e8c8868ddee68ca08
LeorFinkelberg <leor.finkelberg@yandex.ru>
2024-06-17 00:12:34 +0300

    docs: udpate doc-string for `adjusted_r2` function

--- pyspark_expand/metrics/metrics.py
+++ pyspark_expand/metrics/metrics.py
@@ -1,14 +1,17 @@
+import typing as t
+
 from pyspark.ml.evaluation import RegressionEvaluator
 
 
 def adjusted_r2(
-    pred: "DataFrame",
+    dataset: "DataFrame",
     label_col: str = "label",
     feature_col: str = "features",
     prediction_col: str = "prediction",
+    params: t.Optional[t.Dict] = None,
 ) -> float:
     """
-    Compute adjusted R^2.
+    Computes adjusted R^2.
 
     R^2 is a biased estimate since
     the number of features influences the value.
@@ -17,17 +20,35 @@ def adjusted_r2(
     it is good to adjust R^2 value based on the number of
     features in the model.
 
-    :return:
+    Parameters
+    ----------
+    dataset: :py:class:`pyspark.sql.DataFrame`
+        A dataset that contains labels/observations and predictions
+    label_col: str, optional
+        Target column name (default: "label")
+    feature_col: str, optional
+        Feature names (default: "features")
+    prediction_col: str, optional
+        Prediction column name (default: "prediction")
+    params: dict, optional
+        An optional param map that overrides embedded params
+
+    Returns
+    -------
+    float
+        metric
     """
-    METRIC_NAME = "r2"
+
+    _METRIC_NAME = "r2"
 
     evaluator = RegressionEvaluator(
         labelCol=label_col,
         predictionCol=prediction_col,
-        metricName=METRIC_NAME,  # noqa
+        metricName=_METRIC_NAME,  # noqa
     )
-    r2 = evaluator.evaluate(pred)
-    N = pred.count()
-    p = len(pred.select(feature_col).first().asDict()[feature_col])
 
-    return 1 - ((1 - r2**2) * (N - 1) / (N - p - 1))
+    r2 = evaluator.evaluate(dataset, params=params)
+    n = dataset.count()
+    p = len(dataset.select(feature_col).first().asDict()[feature_col])
+
+    return 1 - ((1 - r2**2) * (n - 1) / (n - p - 1))
